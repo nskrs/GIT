921c36200bbb503f87a7332914dc4ed56a2e9977
nskrs <nitinkolhe051>
2023-02-24 23:05:02 +0530

    Adding second and third file

---
+++ domcode.js
@@ -0,0 +1,13 @@
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent='Hello';
+// items[1].style.fontWeight ='bold';
+// items[1].style.backgroundColor='green';
+
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent= 'ADD ITEM';
+titles[0].style.fontWeight='bold';
+titles[0].style.backgroundColor='green'
