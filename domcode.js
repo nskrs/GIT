ee66d444adca5346308ce64b5d375ddb30b4b892
nskrs <nitinkolhe051>
2023-02-25 01:04:40 +0530

    get element change

--- domcode.js
+++ domcode.js
@@ -2,19 +2,19 @@ var items=document.getElementsByClassName('list-group-item');
 console.log(items);
 // console.log(items[1]);
 // items[1].textContent='Hello';
-items[2].style.fontWeight ='bold';
-items[2].style.backgroundColor='green';
-items[0].style.fontWeight ='bold';
-items[1].style.fontWeight ='bold';
-items[3].style.fontWeight ='bold';
-var item = document.querySelector('.list-group-item');
-item.style.color='pink';
-var item = document.querySelector('.list-group-item');
-item.style.color='pink';
-var lastItem = document.querySelector('.list-group-item:last-child');
-lastItem.style.color='blue';
-var secondItem = document.querySelector('.list-group-item : nthchild(2)');
-secondItem.style.color='red';
+// items[2].style.fontWeight ='bold';
+// items[2].style.backgroundColor='green';
+// items[0].style.fontWeight ='bold';
+// items[1].style.fontWeight ='bold';
+// items[3].style.fontWeight ='bold';
+// var item = document.querySelector('.list-group-item');
+// item.style.color='pink';
+// var item = document.querySelector('.list-group-item');
+// item.style.color='pink';
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color='blue';
+// var secondItem = document.querySelector('.list-group-item : nthchild(2)');
+// secondItem.style.color='red';
 var titles = document.querySelectorAll('.title');
 
 console.log(titles);
@@ -22,3 +22,26 @@ titles[0].textContent= 'ADD ITEM';
 
 titles[0].style.fontWeight='bold';
 titles[0].style.backgroundColor='green'
+
+// getElementsByClassName//
+var items = document.getElementsByClassName('.list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='Hello';
+items[1].style.fontWeight='bold';
+items[1].style.backgroundColor='yellow';
+
+for(var i=0; i<items.length;i++){
+    items[i].style.backgroundColor='#f4f4f4';
+}
+//get elemtnsbytagname
+var li = document.getElementsByTagName('.li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='Hello';
+li[1].style.fontWeight='bold';
+li[1].style.backgroundColor='yellow';
+
+for(var i=0; i<ili.length;i++){
+  li[i].style.backgroundColor='#f4f4f4';
+}
