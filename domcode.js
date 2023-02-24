36e5a6985c47a5d3a4cbcdd5d790d2c345f874a2
nskrs <nitinkolhe051>
2023-02-25 00:21:31 +0530

    changes done ++

--- domcode.js
+++ domcode.js
@@ -1,10 +1,20 @@
-// var items=document.getElementsByClassName('list-group-item');
-// console.log(items);
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
 // console.log(items[1]);
 // items[1].textContent='Hello';
-// items[1].style.fontWeight ='bold';
-// items[1].style.backgroundColor='green';
-
+items[2].style.fontWeight ='bold';
+items[2].style.backgroundColor='green';
+items[0].style.fontWeight ='bold';
+items[1].style.fontWeight ='bold';
+items[3].style.fontWeight ='bold';
+var item = document.querySelector('.list-group-item');
+item.style.color='pink';
+var item = document.querySelector('.list-group-item');
+item.style.color='pink';
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color='blue';
+var secondItem = document.querySelector('.list-group-item : nthchild(2)');
+secondItem.style.color='red';
 var titles = document.querySelectorAll('.title');
 
 console.log(titles);
